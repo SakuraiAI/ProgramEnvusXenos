e3d09b1b0a7bd2888c7e0ec373c5f0788469ca0a
Filip Bukovsky <sakurahaise@Filip--MacBook-Pro.local>
2026-06-10 11:56:15 +0200

    Add Program Envus Xenos landing page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
   base: "./",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        systems: resolve(__dirname, "systems.html"),
+        portfolio: resolve(__dirname, "portfolio.html"),
+        marika: resolve(__dirname, "marika.html"),
+        contact: resolve(__dirname, "contact.html"),
+        portfolioUnreal: resolve(__dirname, "portfolio-unreal.html"),
+        portfolioBlender: resolve(__dirname, "portfolio-blender.html"),
+        portfolioVscode: resolve(__dirname, "portfolio-vscode.html"),
+      },
+    },
+  },
 });
