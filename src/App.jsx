895786bb0b8eb32b6ba1ad167bf69cf11da40b1a
SakuraiAI <SakuraiAI@users.noreply.github.com>
2026-07-16 16:37:19 +0200

    Rename Xeno character image

--- src/App.jsx
+++ src/App.jsx
@@ -175,7 +175,7 @@ const INTELLIGENCE_CARDS = [
     href: "./portfolio-unreal.html",
     cta: "Discover Xeno",
     accent: "silver",
-    image: "./characters/xeno.jpg",
+    image: "./characters/xeno-character.jpg",
     imagePosition: "center top",
     contentSide: "right",
   },
